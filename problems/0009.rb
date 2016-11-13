ca03b657603efda5f14b14276c842adf53e38779
Fujisawa Tsutomu <rokugahanfuji@live.jp>
2016-11-13 12:25:12 +0000

    0009

--- problems/0009.rb
+++ problems/0009.rb
@@ -1,14 +1,37 @@
-value = 3
-primeList = Array.new().push(2)
-while value < 100000 do
-    for num in primeList do
-        if value % num == 0 then
-            break
-        end
-        if num == primeList.last and value % num != 0 then
-            primeList.push(value)
-            break
-        end
+#Sieve of Eratosthenes
+searchList = Array.new(1000001,true)
+primeList = Array.new()
+searchList[0] = searchList[1] = false
+i = 2
+
+while i < 1000 do
+    if searchList[i] == false then
+        i += 1
+        next
     end
-    value += 2
-end 
+    searchList[i] = false
+    primeList.push(i)
+    j = 2
+    while (remove = j*i) < 1000000 do
+        searchList[remove] = false
+        j += 1
+    end
+    i += 1
+end
+
+for i in 1000...1000000 do
+    if searchList[i] == true then
+        primeList.push(i)
+    end
+end
+primeList.sort!
+primeList.push(10000001);
+
+while line = gets do
+    count = 0
+    x = line.to_i
+    while primeList[count] <= x do
+        count += 1
+    end
+    puts count
+end
